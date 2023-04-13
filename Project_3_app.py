0b313bd5d917c81b72af30d7f38ebf53d7b4428b
Justin DeMaagd <Justin@iMac.local>
2023-04-13 06:18:40 -0700

    Added dropdown menus for hotel and flights

--- Project_3_app.py
+++ Project_3_app.py
@@ -1,37 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
-from flask_cors import CORS
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime
 
+conn = sqlite3.connect("/Users/Justin/Documents/Bootcamp_Home folder/Project_3_JD/project3.db")
+conn.row_factory = sqlite3.Row
+
+#QUERY HOTEL DATA
+cur = conn.cursor()
+cur.execute("SELECT Name FROM hotels_geo")
+Hotel = cur.fetchall()
+
+#QUERY AIRLINE DATA
+cur2 = conn.cursor()
+cur2.execute("SELECT Departure, Airline, Price FROM tickets")
+Airline = cur2.fetchall()
+
+#CODE DOES NOT WORK!
+#cur3 = conn.cursor()
+#cur3.execute("SELECT time, tvag_farh FROM meteostat_data" )
+#time = []
+#tvag_farh = []
+#for row in cur.fetchall():
+    #time.append(row[0])
+    #tvag_farh.append(row[1])
 
 app = Flask(__name__)
-CORS(app, resources={r"{r"/api/*": ": {"origins": ["http://localhost:8001"]}})
-
-def get_db():
-    conn = sqlite3.connect("/Users/Justin/Documents/Bootcamp_Home folder/Project_3_JD/project3.db")
-    conn.row_factory = sqlite3.Row
-    return conn
-
 
 @app.route('/')
-@app.route('/index')
-def index():
-    sql ="SELECT Departure FROM tickets"
-    conn = get_db()
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    result= [dict(row) for row in cursor.fetchall()]
-    conn.close()
-    return result
-
-@app.route('/hotel_loc')
+@app.route('/index.html')
 def hotel():
-    sql ="SELECT * FROM hotels_geo"
-    conn = get_db()
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    result= [dict(row) for row in cursor.fetchall()]
-    conn.close()
-    return render_template("index.html", result=result)
+    return render_template('index.html', Hotel=Hotel, Airline= Airline)
+
+#DO NOT UNHASH....CODE CAUSES APP TO CRASH
+#@app.route('/graph')
+#def graph():
+    
+   
+    #plt.plot_date(time, tvag_farh)   
+    #plt.title("Historical Temperature")
+    #plt.show()
+
+
 
 
 
